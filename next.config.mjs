39fff81310149bf7030e52ddcca3eab7bfb66465
Berredjem Salah Eddine <berredjem.salah.eddine@gmail.com>
2025-10-17 01:22:27 +0100

    feat(config): optimize build and dependencies configuration
    Improved config.mjs for better performance and updated package.json to enhance dependency management and reduce build time.

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,8 @@ const nextConfig = {
         minimumCacheTTL: 31536000,
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+        domains: ['res.cloudinary.com'],
+
     },
     // Add video optimization
     webpack(config) {
