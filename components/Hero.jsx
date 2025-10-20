bf6676f3b4571cbfbdfc742907d8a76e85d28acc
Berredjem Salah Eddine <berredjem.salah.eddine@gmail.com>
2025-10-20 01:29:09 +0100

    feat(section-Hero):upadating the CTA buttons

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,14 +6,14 @@ export default function Hero(){
     return(
         <main id="hero" className="h-screen w-full  grid content-between justify-items-center lg:content-center text-white font-bold pt-10 px-5 lg:px-20 xl:px-48 z-0">
             <section className=" w-full grid justify-items-center content-center lg:justify-items-start">
-                <article>
+                <article className="">
                     <span className='text-[88px] lg:text-[100px] grid content-center  h-max justify-items-start w-full' style={{textShadow:'0 0 15px rgb(0,0,0)'}}>BERREDJEM <span className="-translate-y-12">SALAH</span></span>
                     <span className='text-[27px] lg:text-[30px] px-1 w-full -translate-y-[130px] lg:-translate-y-[140px] flex items-center justify-end' style={{textShadow:'0 0 10px rgb(255,255,255)'}}>-3D PORTFOLIO</span>
+                    <div role="button" aria-label="CTA buttons" className="flex items-center h-max w-full  justify-center gap-3 -translate-y-10">
+                        <ScrollLink to="footer" smooth={true} duration={600} offset={-100} className="flex items-center gap-2 cursor-pointer hover:opacity-70 duration-300"><span className="bg-white text-[#0db988] py-1 px-10 rounded-2xl border-[1px] text-[20px] hover:px-16 duration-300 hover:bg-gray-400 hover:text-indigo-950 transition-all hover:tracking-widest" style={{boxShadow:'0 0 10px #ffffff'}}>GET IN TOUCH</span></ScrollLink>
+                        <Link href="/resume.pdf" download="Berredjem-Salah-Resume.pdf" className="text-[15px] underline hover:tracking-widest duration-300 hover:text-gray-400 flex items-center gap-2" style={{textShadow:'0 0 5px #000000'}} target="_blank" rel="noopener noreferrer">download resume<i className="fas fa-download"></i></Link>
+                    </div>
                 </article>
-            </section>
-            <section className="flex items-center h-max w-full justify-center gap-3 -translate-y-10">
-                <ScrollLink to="footer" smooth={true} duration={600} offset={-100} className="flex items-center gap-2 cursor-pointer hover:opacity-70 duration-300"><span className="bg-white text-[#0db988] py-1 px-10 rounded-2xl border-[1px] text-[20px] hover:px-16 duration-300 hover:bg-gray-400 hover:text-indigo-950 transition-all hover:tracking-widest" style={{boxShadow:'0 0 10px #ffffff'}}>GET IN TOUCH</span></ScrollLink>
-                <Link href="/resume.pdf" download="Berredjem-Salah-Resume.pdf" className="text-[15px] underline hover:tracking-widest duration-300 hover:text-gray-400 flex items-center gap-2" style={{textShadow:'0 0 5px #000000'}} target="_blank" rel="noopener noreferrer">download resume<i className="fas fa-download"></i></Link>
             </section>
         </main>
     )
