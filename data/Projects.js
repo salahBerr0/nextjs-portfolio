d328102cef57056d91ee9907c95513b33c278bce
Berredjem Salah Eddine <berredjem.salah.eddine@gmail.com>
2025-10-20 01:49:29 +0100

    feat(data):inserting the media content to the datas like the tools images and the projects videos to test loading images from cloudinary
    Including: Projects.js, Tools.js, Skills.js and the next.config.mjs for the best loading of the images and the automatic images formats converting from webp to avif based on the browser ability.

--- data/Projects.js
+++ data/Projects.js
@@ -1,12 +1,24 @@
 export const Projects = [
-  { _id: '1', projTitle: 'Visual identity brand',projTagLine: "tagline1 dfjkasdjfkdfjkdjiiefk",projDescription: 'des1',projCategory: ['cat11'],projTechStack: [1,2,3],projLiveDemoLink: 'demoLink1',projSourceCodeLink: 'sourcelink1',projLikesCount: 4,
-    projImageUrls: [{priority: 1, url: "/imgs/video-poster.jpg"}, {priority: 3, url: "/imgs/github.png"}, {priority: 2, url: "/imgs/linkedIN.png"}],
-    projVideosUrls: ["/vds/bsVd2.mp4"]
+  { _id: '1', projTitle: 'Visual identity brand',projTagLine: "tagline1 dfjkasdjfkdfjkdjiiefk",projCategory: ['cat11'],projTechStack: [1,2,3],projLiveDemoLink: 'demoLink1',projSourceCodeLink: 'sourcelink1',projLikesCount: 4,projDescription: 'des1',
+    projImageUrls: [{priority: 1, url: "/imgs/video-poster.webp"}, {priority: 3, url: "/imgs/github.webp"}, {priority: 2, url: "/imgs/linkedIN.webp"}],
+    projVideosUrls: ["/vds/bsVd2.webm"]
   },
   
-  { _id: '2', projTitle: 'bootstrap',projTagLine: "tagline2",projDescription: ' des2',projCategory: ['cat21','cat22'],projTechStack: [4,5,6],projLiveDemoLink: 'demoLink2',projSourceCodeLink: 'sourcelink2',projLikesCount: 2,
-    projImageUrls: [{priority: 1, url: "/imgs/Bootstrap.png"}, {priority: 2, url: "/imgs/Canva.png"},{priority: 4, url: "/imgs/Eclipse.png"}],
-    projVideosUrls: [{priority: 3, url: "/vds/bsVd.mp4"}]
+  { _id: '2', projTitle: 'AYMN - Personal Identity',projTagLine: "Unique logo capturing identity, and creativity.",projCategory: ['Graphic Design'],projTechStack: [3,4,22,24],projLikesCount: 0,projDescription: 'My personal visual identity is reflected in two distinct logo designs, each rooted in meaning and creative experimentation.This project explores my second logo, which represents my nickname, AYMN. Here, the letters A, Y, M, and N are seamlessly fused into a modern, balanced composition. Its sharp angles and curves embody both precision and creativity, with a versatile monochrome style suited for professional use.The idea for these logos began as a personal search for a brand identity. I sketched initial concepts on my iPhone’s Paint app, exploring ideas over a month before refining them in Illustrator. Through multiple iterations, I shaped the final designs shown here.Together, these logos showcase my approach to branding: blending personal meaning with professional design to create marks that are simple, versatile, and timeless.',
+    projImageUrls: [
+      {priority: 1, url: "https://res.cloudinary.com/dzpj6c5pw/image/upload/v1760833685/1_gsahpd.webp"},
+      {priority: 4, url: "https://res.cloudinary.com/dzpj6c5pw/image/upload/v1760833685/4_qo0orh.webp"},
+      {priority:10 , url: "https://res.cloudinary.com/dzpj6c5pw/image/upload/v1760834267/10_rk3iyb.webp"},
+      {priority:11 , url: "https://res.cloudinary.com/dzpj6c5pw/image/upload/v1760834269/11_gp7hin.webp"}
+    ],
+    projVideosUrls: [
+      {priority: 2, url: "https://res.cloudinary.com/dzpj6c5pw/video/upload/v1760833684/2_y5hhcm.webm"},
+      {priority: 3, url: "https://res.cloudinary.com/dzpj6c5pw/video/upload/v1760833690/3_n8jpck.webm"},
+      {priority:5 , url: "https://res.cloudinary.com/dzpj6c5pw/video/upload/v1760833748/5_wkqm2x.webm"},
+      {priority:6 , url: "https://res.cloudinary.com/dzpj6c5pw/video/upload/v1760833863/6_p3drql.webm"},
+      {priority:7 , url: "https://res.cloudinary.com/dzpj6c5pw/video/upload/v1760833872/7_vtety6.webm"},
+      {priority:8 , url: "https://res.cloudinary.com/dzpj6c5pw/video/upload/v1760833876/8_ybms6d.webm"}
+    ]
   },
 
 
--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,14 @@ const nextConfig = {
     compress: true,
     poweredByHeader: false,
     images: {
+        unoptimized: false,
         formats: ['image/avif', 'image/webp'],
         minimumCacheTTL: 31536000,
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
         domains: ['res.cloudinary.com'],
-
+        dangerouslyAllowSVG: true,
+        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     },
     // Add video optimization
     webpack(config) {
